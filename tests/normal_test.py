e4ffebeb02eb853fc1732fe6284551fe16954a9b
Barne Kleinen <barne.kleinen@htw-berlin.de>
2023-06-13 14:41:23 +0200

    add example test for item

--- tests/normal_test.py
+++ tests/normal_test.py
@@ -3,4 +3,8 @@ from tests.settings import *
 # create tests for normal items here...
 
 def test_something():
-    pass
+    item = Item("name item", 5, 20)
+    gilded_rose = GildedRose([item])
+    gilded_rose.update_quality()
+    assert item.sell_in == 4
+    assert item.quality == 19
