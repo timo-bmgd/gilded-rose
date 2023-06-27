5a941717e95441aa46bfd1ad982661fea5a9d324
ultratynoz <tynoz@web.de>
2023-06-27 19:31:26 +0200

    refactored single methods

--- gilded_rose/refactored/gilded_rose.py
+++ gilded_rose/refactored/gilded_rose.py
@@ -6,120 +6,61 @@ class ItemTypes:
     BACK = "Backstage passes to a TAFKAL80ETC concert"
 
 
+def update_normal(item):
+    if item.quality > 0:
+        item.quality = item.quality - 1
+    item.sell_in = item.sell_in - 1
+    if item.sell_in < 0:
+        if item.quality > 0:
+            item.quality = item.quality - 1
+
+
+def update_brie(item):
+    if item.quality < 50:
+        item.quality = item.quality + 1
+    item.sell_in = item.sell_in - 1
+    if item.sell_in < 0:
+        if item.quality < 50:
+            item.quality = item.quality + 1
+
+
+def update_backstage(item):
+    if item.quality < 50:
+        item.quality = item.quality + 1
+        if item.sell_in < 11:
+            if item.quality < 50:
+                item.quality = item.quality + 1
+        if item.sell_in < 6:
+            if item.quality < 50:
+                item.quality = item.quality + 1
+    item.sell_in = item.sell_in - 1
+    if item.sell_in < 0:
+        item.quality = item.quality - item.quality
+def update_sulfuras(item):
+    pass
+
+
 class GildedRose(object):
 
     def __init__(self, items):
         self.items = items
 
-    def update_normal(self, item):
-        if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-            if item.quality > 0:
-                if item.name != "Sulfuras, Hand of Ragnaros":
-                    item.quality = item.quality - 1
-        else:
-            if item.quality < 50:
-                item.quality = item.quality + 1
-                if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                    if item.sell_in < 11:
-                        if item.quality < 50:
-                            item.quality = item.quality + 1
-                    if item.sell_in < 6:
-                        if item.quality < 50:
-                            item.quality = item.quality + 1
-        if item.name != "Sulfuras, Hand of Ragnaros":
-            item.sell_in = item.sell_in - 1
-        if item.sell_in < 0:
-            if item.name != "Aged Brie":
-                if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                    if item.quality > 0:
-                        if item.name != "Sulfuras, Hand of Ragnaros":
-                            item.quality = item.quality - 1
-                else:
-                    item.quality = item.quality - item.quality
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
 
-    def update_brie(self, item):
-        if item.quality < 50:
-            item.quality = item.quality + 1
-            item.sell_in = item.sell_in - 1
-        if item.sell_in < 0:
-            if item.quality < 50:
-                item.quality = item.quality + 1
-
-    def update_backstage(self, item):
-        if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-            if item.quality > 0:
-                if item.name != "Sulfuras, Hand of Ragnaros":
-                    item.quality = item.quality - 1
-        else:
-            if item.quality < 50:
-                item.quality = item.quality + 1
-                if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                    if item.sell_in < 11:
-                        if item.quality < 50:
-                            item.quality = item.quality + 1
-                    if item.sell_in < 6:
-                        if item.quality < 50:
-                            item.quality = item.quality + 1
-        if item.name != "Sulfuras, Hand of Ragnaros":
-            item.sell_in = item.sell_in - 1
-        if item.sell_in < 0:
-            if item.name != "Aged Brie":
-                if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                    if item.quality > 0:
-                        if item.name != "Sulfuras, Hand of Ragnaros":
-                            item.quality = item.quality - 1
-                else:
-                    item.quality = item.quality - item.quality
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-
-    def update_sulfuras(self, item):
-        if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-            if item.quality > 0:
-                if item.name != "Sulfuras, Hand of Ragnaros":
-                    item.quality = item.quality - 1
-        else:
-            if item.quality < 50:
-                item.quality = item.quality + 1
-                if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                    if item.sell_in < 11:
-                        if item.quality < 50:
-                            item.quality = item.quality + 1
-                    if item.sell_in < 6:
-                        if item.quality < 50:
-                            item.quality = item.quality + 1
-        if item.name != "Sulfuras, Hand of Ragnaros":
-            item.sell_in = item.sell_in - 1
-        if item.sell_in < 0:
-            if item.name != "Aged Brie":
-                if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                    if item.quality > 0:
-                        if item.name != "Sulfuras, Hand of Ragnaros":
-                            item.quality = item.quality - 1
-                else:
-                    item.quality = item.quality - item.quality
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
 
     def update_quality(self):
         for item in self.items:
-            match item:
+            match item.name:
                 case ItemTypes.BRIE:
-                    self.update_brie(item)
+                    update_brie(item)
                     continue
                 case ItemTypes.BACK:
-                    self.update_backstage(item)
+                    update_backstage(item)
                     continue
                 case ItemTypes.SULF:
-                    self.update_sulfuras(item)
+                    update_sulfuras(item)
                     continue
                 case _:
-                    self.update_normal(item)
+                    update_normal(item)
                     continue
 
 
